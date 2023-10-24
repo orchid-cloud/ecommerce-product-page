83d6b25dc7891c13c55c34dc49f6255fe02fe011
NZ <n.prokopyk@gmail.com>
2023-10-24 10:38:24 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        orange_font: "var(--orange-font)",
+      },
+    },
+  },
+  plugins: [],
+};
